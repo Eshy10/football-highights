f7ec3e5ec64ca827789d43f36d8f6f9f1f9108b8
Eshy10 <adewalebose15@gmail.com>
2020-10-28 00:04:48 +0100

    add header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Container from '@material-ui/core/Container';
 import Navbar from './components/nav/Nav';
+import CarouselHeader from './components/header/Header'
 
 function App() {
   return (
     <div className="App">
     <Container>
     <Navbar/>
+    <CarouselHeader/>
     </Container>
     </div>
   );
